e91773a3e0760bbd1f86f77fc286be74d9fd6886
Neal Iyer <the.neal.returns@gmail.com>
2017-10-28 21:39:11 +0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require ('express');
+var path = require('path');
+var app = express();
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit!');
+		next();
+
+	},
+	logger: function(req, res, next){
+		
+		console.log('Request: ' + new Date().toString()+ ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+// app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication,  function(req,res){
+
+res.send('About Us');
+} );
+
+var correctedpath = path.normalize(__dirname); 
+
+app.use(express.static( correctedpath +'/public'));
+app.listen(3000, function(){
+	console.log('Express Server Started!');
+});
+
